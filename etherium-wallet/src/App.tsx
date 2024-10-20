386213b622fe36a532cb8600c82df56645474cd8
pratikprajapati0708 <prajapatipratik292@gmail.com>
2024-10-20 21:38:50 +0530

    added input type

--- etherium-wallet/src/App.tsx
+++ etherium-wallet/src/App.tsx
@@ -3,9 +3,10 @@ import './App.css'
 function App() {
 
   return (
-    <>
-    
-    </>
+    <div>
+        <input type="text" placeholder='Address....' />
+        <button>Send 0.1 ETH</button>
+    </div>
   )
 }
 
